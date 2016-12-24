f1e0daa3712556a87f374036dc74ac5ca57e0ad8
Kirill Pilyugin <kirillpilyugin@gmail.com>
2016-12-25 02:15:40 +0300

    clean-up

--- src/main/java/launcher/BenchmarkLauncher.java
+++ src/main/java/launcher/BenchmarkLauncher.java
@@ -20,13 +20,12 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.*;
-import java.util.function.LongConsumer;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class BenchmarkLauncher {
-
-  private static final long PACKET_LOSS_PENALTY = 100;
-
   private final ExecutorService clientExecutor = Executors.newCachedThreadPool();
   private final BenchmarkParams params;
 
